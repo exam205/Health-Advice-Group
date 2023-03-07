50928512e06255230017c109b3bfebc4807ecfb0
exam205 <123750076+exam205@users.noreply.github.com>
2023-03-07 11:33:50 +0000

    First Commit
    Base Navbar done and included both JQuery and Bootstrap

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <?php include 'modules/links.php'; ?> 
+</head>
+<body>
+    <?php include 'modules/navbar.php'; ?>
+</body>
+</html>
---
+++ modules/navbar.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Health Advice Group</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Weather Forecast</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Air Quality Data</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Advice</a>
+        </li>
+        <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) { ?> <!-- If user is logged in navbar changes -->
+            <li class="nav-item">
+                <a class="nav-link" href="#">Health Tracker</a>
+            </li>            
+            <li class="nav-item">
+                <a class="nav-link" href="#">My Account</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Logout</a>
+            </li>
+
+        <?php } else { ?>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login / Sign Up</a>
+            </li>
+        <?php } ?>
+      </ul>
+    </div>
+  </div>
+</nav>
